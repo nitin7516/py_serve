c38407ccc9a38e4de45a661ae5abbfee9cc894e8
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-11 15:43:14 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+# from time import time, sleep
+# while True:
+#     sleep(60 - time() % 60)
+import os
+import http.server
+import socketserver
+
+PORT = int(os.getenv('VCAP_APP_PORT', '8000'))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+from time import sleep 
+for i in range(6):
+    msg = "Hello World!"
+    sleep(2) 
+    print(msg)
+print("serving at port", PORT)
+httpd.serve_forever()
