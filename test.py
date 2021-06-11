d68295edb25aae5ad54e026d717dc6ee224c854c
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-11 20:38:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ httpd = socketserver.TCPServer(("", PORT), Handler)
 from time import sleep 
 for i in range(6):
     msg = "Hello World!"
-    sleep(2) 
+#    sleep(2) 
     print(msg)
 print("serving at port", PORT)
 httpd.serve_forever()
