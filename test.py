209cc5dd21d5fcb7b79fbddc6aef86061589e845
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-12 21:40:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,19 @@
-from math import sqrt
-from time import sleep
-import flask
+from flask import Flask
 import os
-import http.server
-import socketserver
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+# Port number is required to fetch from env variable
+# http://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#PORT
 
+cf_port = os.getenv("PORT")
 
-def get_message():
-    '''this could be any function that blocks until data is ready'''
-    time.sleep(1.0)
-    s = time.ctime(time.time())
-    return s
-
+# Only get method by default
 @app.route('/')
-def root():
-    return render_template('index.html')
-
-@app.route('/stream')
-def stream():
-    def eventStream():
-        while True:
-            # wait for source data to be available, then push it
-            yield 'data: {}\n\n'.format(get_message())
-    return Response(eventStream(), mimetype="text/event-stream")
-
+def hello():
+    return 'Hello World'
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+	if cf_port is None:
+		app.run(host='0.0.0.0', port=5000, debug=True)
+	else:
+		app.run(host='0.0.0.0', port=int(cf_port), debug=True)
