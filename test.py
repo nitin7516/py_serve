69269c73bc11e2f9aa121c4bd6cf7bc821a5c3a8
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-12 20:49:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,10 +10,11 @@ PORT = int(os.getenv('VCAP_APP_PORT', '8000'))
 Handler = http.server.SimpleHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
+
 from time import sleep 
-for i in range(6):
+for i in range(10):
     msg = "Hello World!"
-   sleep(2) 
+    sleep(2) 
     print(msg)
 print("serving at port", PORT)
 httpd.serve_forever()
