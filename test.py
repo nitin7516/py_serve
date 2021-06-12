6d5cbec0ba7e196e2a58c387da9de8cdbc80b8b6
nitin7516 <40827296+nitin7516@users.noreply.github.com>
2021-06-12 21:53:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,23 @@ app = Flask(__name__)
 
 cf_port = os.getenv("PORT")
 
-# Only get method by default
+def get_message():
+    '''this could be any function that blocks until data is ready'''
+    time.sleep(1.0)
+    s = time.ctime(time.time())
+    return s
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+def root():
+    return render_template('index.html')
+
+@app.route('/stream')
+def stream():
+    def eventStream():
+        while True:
+            # wait for source data to be available, then push it
+            yield 'data: {}\n\n'.format(get_message())
+    return Response(eventStream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':
 	if cf_port is None:
